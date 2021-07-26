c53529aea8114a3558909596d463f5e22c90be12
Fblfbl <55172480+Fblfbl@users.noreply.github.com>
2021-07-26 22:22:30 +0300

    add problems

--- src/movies/movie.repository.ts
+++ src/movies/movie.repository.ts
@@ -3,7 +3,7 @@ import { Movie } from './movie.entity';
 
 @EntityRepository(Movie)
 export class MovieRepository extends Repository<Movie> {
-  async findAllByUser(id: number) {
+  async findAllByUser(id: number) { // Not same work as second method, but same as third
     return this.find({
       relations: ['userDetails'],
       join: {
@@ -15,4 +15,122 @@ export class MovieRepository extends Repository<Movie> {
       },
     });
   }
+  
+  // async findAllByUser(id: number) { // Works as it should
+  //   return (
+  //     this.createQueryBuilder('movie')
+  //       .leftJoinAndSelect(
+  //         'movie.userDetails',
+  //         'userDetails',
+  //         `movie.id = userDetails.movieId AND userDetails.userId = ${id}`,
+  //       )
+  //       .getMany()
+  //   );
+  // }
+  
+  // async findAllByUser(id: number) { // Works as first method, not as second. Why?
+  //   return this.createQueryBuilder('movie')
+  //     .leftJoinAndSelect('movie.userDetails', 'userDetails')
+  //     .where(`movie.id = userDetails.movieId`)
+  //     .andWhere('userDetails.userId = :userId', { userId: id })
+  //     .getMany();
+  // }
+
+  //First and third methods return
+  //   [
+  //     {
+  //         "id": 1,
+  //         "title": "test1",
+  //         "genre": "test1",
+  //         "userDetails": {
+  //             "id": 1,
+  //             "isFavorite": false,
+  //             "isWatched": true,
+  //             "isInWatchlist": false,
+  //             "watchingDate": "2021-07-25T19:29:18.510Z",
+  //             "userId": 1,
+  //             "movieId": 1
+  //         }
+  //     },
+  //     {
+  //         "id": 2,
+  //         "title": "test2",
+  //         "genre": "test2",
+  //         "userDetails": {
+  //             "id": 2,
+  //             "isFavorite": true,
+  //             "isWatched": true,
+  //             "isInWatchlist": false,
+  //             "watchingDate": "2021-07-26T13:08:21.533Z",
+  //             "userId": 1,
+  //             "movieId": 2
+  //         }
+  //     },
+  //     {
+  //         "id": 3,
+  //         "title": "test3",
+  //         "genre": "test3",
+  //         "userDetails": {
+  //             "id": 3,
+  //             "isFavorite": true,
+  //             "isWatched": true,
+  //             "isInWatchlist": true,
+  //             "watchingDate": "2021-07-26T13:09:11.852Z",
+  //             "userId": 1,
+  //             "movieId": 3
+  //         }
+  //     }
+  // ]
+  
+  // Second method returns (What i needed)
+  // [
+  //     {
+  //         "id": 1,
+  //         "title": "test1",
+  //         "genre": "test1",
+  //         "userDetails": {
+  //             "id": 1,
+  //             "isFavorite": false,
+  //             "isWatched": true,
+  //             "isInWatchlist": false,
+  //             "watchingDate": "2021-07-25T19:29:18.510Z",
+  //             "userId": 1,
+  //             "movieId": 1
+  //         }
+  //     },
+  //     {
+  //         "id": 2,
+  //         "title": "test2",
+  //         "genre": "test2",
+  //         "userDetails": {
+  //             "id": 2,
+  //             "isFavorite": true,
+  //             "isWatched": true,
+  //             "isInWatchlist": false,
+  //             "watchingDate": "2021-07-26T13:08:21.533Z",
+  //             "userId": 1,
+  //             "movieId": 2
+  //         }
+  //     },
+  //     {
+  //         "id": 3,
+  //         "title": "test3",
+  //         "genre": "test3",
+  //         "userDetails": {
+  //             "id": 3,
+  //             "isFavorite": true,
+  //             "isWatched": true,
+  //             "isInWatchlist": true,
+  //             "watchingDate": "2021-07-26T13:09:11.852Z",
+  //             "userId": 1,
+  //             "movieId": 3
+  //         }
+  //     },
+  //     {
+  //         "id": 4,
+  //         "title": "test4",
+  //         "genre": "test4",
+  //         "userDetails": null
+  //     }
+  // ]
 }
